aa6df83fcba3a036fca9175806c064afb236b9df
dzayeate <mr.x685@gmail.com>
2022-10-25 16:51:28 +0700

    Post Model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,11 +2,11 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">Halaman Post</h1>
     <article>
-        <h2>{{ $post["title"] }}</h2>
-        <h5>By: {{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+        <h1 class="mb-5">{{ $post->title }}</h1>
+
+        {!! $post->body !!}
+
     </article>
 
     <a href="/blog">Back to Posts</a>

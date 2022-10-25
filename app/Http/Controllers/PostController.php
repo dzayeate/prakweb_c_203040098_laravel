a77f62fd840cc2e05c44b5b36b6b0107e26146f0
dzayeate <mr.x685@gmail.com>
2022-10-25 21:33:13 +0700

    N+1 Problem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,9 +10,9 @@ class PostController extends Controller
     public function index()
     {
         return view('posts', [
-            "title" => "Posts",
-//            "posts" => Post::all()
-            "posts" => Post::latest()->get(),
+            "title" => "All Posts",
+            // "posts" => Post::all(),
+            "posts" => Post::with(['author', 'category'])->latest()->get(),
         ]);
     }
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,22 +2,17 @@
 
 @section('container')
 
-@foreach ($posts as $post)
-    <article class="mb-5 border-bottom pb-4">
-        <h2>
-            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
-        </h2>
+    <h1 class="mb-5">{{ $title }}</h1>
 
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    @foreach ($posts as $post)
+        <article class="mb-5 border-bottom pb-4">
+            <h2>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
+            </h2>
+            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-
-
-        <p>{{ $post->excerpt }}</p>
-
-        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
-    </article>
-
-@endforeach
-
-
+            <p>{{ $post->excerpt }}</p>
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
+        </article>
+    @endforeach
 @endsection
